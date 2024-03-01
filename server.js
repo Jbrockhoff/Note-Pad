e0d73b7e1064a4aa9d2bf57edfc2678c0f6797db
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-03-01 11:49:12 -0600

    added app.use functions to server.js, needs work

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = requre('express');
 const path = require('path');
 const noteData = require('./db/db.json');
 const PORT = 3001;
-const app = express();
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
-app.get('/api/notes', (req, res) => res.json(noteData))
+app.get('/api/public', (req, res) => res.json(noteData))
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
