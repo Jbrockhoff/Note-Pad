c528dc25356fe323c08780cbe77cdd036146c102
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-03-01 11:34:27 -0600

    altered server.js, not correct and needs edited

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
+
+app.get('/api/notes', (req, res) => res.json(noteData))
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
