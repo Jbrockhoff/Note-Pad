2212a55b7d0f1f73c2daae07a1d7a9f7cbb805e4
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-03-02 14:36:51 -0600

    removed personal notes from server.js

--- server.js
+++ server.js
@@ -8,17 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(api);
 
-
-
-
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
-
-//landing page has link to notes page
-//Existing notes stored and logged on the left
-//Empty fields exist to enter:
-//    new note
-//    new title
-//when entering new note, 'save' and 'clear' buttons appear in nav at the top
-
