08e991045db107cd6b7b5618efb6d0c5d0c89cbc
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-03-02 13:38:03 -0600

    added router code for retrieving notes.html data to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const path = require("path");
+const fs = require("fs");
 
-  router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+router.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (error, data) => {
+    console.log(error);
+    console.log(data);
+    res.send(data);
   });
+});
 
-  router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-  module.exports = router
+module.exports = router;
