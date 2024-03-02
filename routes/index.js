0980e9418199e25af58003186106c97c2d5cfe24
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-03-02 13:56:01 -0600

    completed code for deleteing notes in injex.js/routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
+const uniqid = require("uniqid")
 
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
@@ -8,9 +9,30 @@ router.get("/notes", (req, res) => {
 
 router.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (error, data) => {
-    console.log(error);
-    console.log(data);
     res.send(data);
+  });
+});
+
+router.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (error, data) => {
+    const notes = JSON.parse(data);
+    const newNote = req.body;
+    newNote.id = uniqid();
+
+    notes.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (error, data) => {
+      res.json(newNote);
+    });
+  });
+});
+
+router.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (error, data) => {
+    const notes = JSON.parse(data);
+    const updatedNotes = notes.filter(note => note.id !== req.params.id)
+    fs.writeFile('./db/db.json', JSON.stringify(updatedNotes), (error, data) => {
+      res.json(200);
+    });
   });
 });
 
