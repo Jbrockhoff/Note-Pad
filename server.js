6d63ba5384e89c23c3ee97f59c0a659bfae6a267
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-03-02 13:27:09 -0600

    added router for notes and index.html

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = requre('express');
-const path = require('path');
-const noteData = require('./db/db.json');
+const express = require("express");
 const PORT = 3001;
+const api = require('./routes/index');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(api);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
-});
 
-app.get('/api/public', (req, res) => res.json(noteData))
+
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
+//landing page has link to notes page
+//Existing notes stored and logged on the left
+//Empty fields exist to enter:
+//    new note
+//    new title
+//when entering new note, 'save' and 'clear' buttons appear in nav at the top
+
